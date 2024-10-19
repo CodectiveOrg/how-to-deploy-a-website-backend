27c768e707989fd7a02ad7862a9deff9e8f6c720
BijanProgrammer <bijaneisapour@gmail.com>
2024-10-20 02:20:34 +0330

    [ADD] cors

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import express from 'express';
+import cors from 'cors';
 
 const PORT = 5000;
 
 async function main(): Promise<void> {
     const app = express();
+
+    app.use(cors());
 
     app.get('/', (req, res) => {
         res.send('Hello, friend!');
