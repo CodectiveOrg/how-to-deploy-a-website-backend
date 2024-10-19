2e9a941ef5a98c7c9a8d94d4b8c064b6fc86912b
BijanProgrammer <bijaneisapour@gmail.com>
2024-10-20 01:04:11 +0330

    [ADD] express

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
+import express from 'express';
+
+const PORT = 5000;
+
 async function main(): Promise<void> {
-    console.log('Hello, friend!');
+    const app = express();
+
+    app.get('/', (req, res) => {
+        res.send('Hello, friend!');
+    });
+
+    app.listen(PORT, () => {
+        console.log(`Listening on port ${PORT}...`);
+    });
 }
 
 main().then();
